c55a44d796bf08c10d3bd1b1ac5013f72881528c
Shinae Lee <gosin77@snu.ac.kr>
2024-10-12 19:04:08 +0900

    feat : exception 생성

---
+++ src/main/java/com/geoparty/spring_boot/global/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.geoparty.spring_boot.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    BAD_REQUEST(400, "잘못된 요청입니다."),
+    UNAUTHORIZED(401,"잘못된 토큰입니다."),
+    INVALID_TOKEN(401, "토큰이 유효하지 않습니다."),
+    NOT_PRIVIEGED(403, "접근 권한이 없습니다."),
+    NOT_FOUND_DATA(404, "데이터를 찾지 못했습니다."),
+    NOT_FOUND(404, "페이지를 찾지 못했습니다."),
+    NOT_ENOUGH_MILEAGE(403,"마일리지가 부족합니다." );
+
+
+    private final int errorCode;
+    private final String errorMsg;
+
+}
