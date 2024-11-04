29f02067ae91aa58f7c1507f906520d5a0100a61
Shinae Lee <gosin77@snu.ac.kr>
2024-11-04 18:03:23 +0900

    refactor : organization api 수정

--- src/main/java/com/geoparty/spring_boot/domain/organization/service/OrgService.java
+++ src/main/java/com/geoparty/spring_boot/domain/organization/service/OrgService.java
@@ -4,13 +4,17 @@ import com.geoparty.spring_boot.domain.organization.dto.request.OrgListRequest;
 import com.geoparty.spring_boot.domain.organization.dto.request.OrgRequest;
 import com.geoparty.spring_boot.domain.organization.dto.response.OrgListResponse;
 import com.geoparty.spring_boot.domain.organization.dto.response.OrgResponse;
-import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 
 public interface OrgService {
-    void createOrganization(OrgRequest request); // 환경 단체 등록
-    OrgListResponse getOrganizations(OrgListRequest request); // 환경 단체 리스트 조회
+    void createOrganization(OrgRequest request, MultipartFile thumbnail, List<MultipartFile> photos, MultipartFile pdf); // 환경 단체 등록
+//    OrgListResponse getOrganizations(OrgListRequest request); // 환경 단체 리스트 조회
 
     OrgResponse getDetail(Long orgId); // 환경 단체 상세 조회
+
+    OrgListResponse getOrganizations();
 
 }
